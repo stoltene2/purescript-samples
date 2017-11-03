7922dc588171fc904ea1314a968e61edb3ad17f4
Eric Stolten <stoltene2@gmail.com>
2017-11-03 07:47:47 -0400

    Add newtype accessors for AddressBook

--- src/AddressBook.purs
+++ src/AddressBook.purs
@@ -7,13 +7,13 @@ import Data.List (List(..), filter, head, nubBy)
 import Data.Maybe (Maybe)
 
 
-type Entry =
+newtype Entry = Entry
   { firstName :: String
   , lastName :: String
   , address :: Address
   }
 
-type Address =
+newtype Address = Address
   { street :: String
   , city :: String
   , state :: String
@@ -22,15 +22,21 @@ type Address =
 
 type AddressBook = List Entry
 
-
-showEntry :: Entry -> String
-showEntry entry = entry.lastName <> ", " <>
-                  entry.firstName <> ": " <>
-                  showAddress entry.address
+address :: String -> String -> String -> Address
+address street city state = Address {street, city, state}
 
 
-showAddress :: Address -> String
-showAddress addr = addr.street <> ", " <> addr.city <> ", " <> addr.state
+instance showEntry :: Show Entry where
+  show (Entry entry) = entry.lastName <> ", " <>
+                       entry.firstName <> ": " <>
+                       show entry.address
+
+instance eqEntry :: Eq Entry where
+  eq (Entry e1) (Entry e2) = e1.firstName == e2.firstName && e1.lastName == e2.lastName
+
+
+instance showAddress :: Show Address where
+  show (Address a) = a.street <> ", " <> a.city <> ", " <> a.state
 
 
 emptyBook :: AddressBook
@@ -45,32 +51,35 @@ findEntry :: String -> String -> AddressBook -> Maybe Entry
 findEntry first last = head <<< filter filterEntry
   where
     filterEntry :: Entry -> Boolean
-    filterEntry e = e.firstName == first && e.lastName == last
+    filterEntry (Entry e) = e.firstName == first && e.lastName == last
 
 
 findEntryByStreet :: String -> AddressBook -> Maybe Entry
 findEntryByStreet street = head <<< filter streetFilter
   where
     streetFilter :: Entry -> Boolean
-    streetFilter e = e.address.street == street
+    streetFilter (Entry e) =
+      let
+        (Address addr) = e.address
+      in addr.street == street
 
 
 printEntry :: String -> String -> AddressBook -> Maybe String
-printEntry first last book = map showEntry (findEntry first last book)
+printEntry first last book = map show (findEntry first last book)
 
 
 removeDuplicates :: AddressBook -> AddressBook
-removeDuplicates = nubBy nameEqual
-  where
-    nameEqual :: Entry -> Entry -> Boolean
-    nameEqual e1 e2 = e1.firstName == e2.firstName && e1.lastName == e2.lastName
+removeDuplicates = nubBy eq
+  -- where
+  --   nameEqual :: Entry -> Entry -> Boolean
+  --   nameEqual e1 e2 = e1.firstName == e2.firstName && e1.lastName == e2.lastName
 
 
 defaultEntry :: Entry
 defaultEntry =
-  { firstName: "Eric"
+  Entry { firstName: "Eric"
   , lastName: "Stolten"
-  , address: { street: "123 foo bar ln"
+  , address: Address { street: "123 foo bar ln"
              , city: "barville"
              , state: "PA"
              }
